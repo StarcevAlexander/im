baea1452e3b6070c3c8df4195bf0611c18823e72
StarcevAlexander <116572797+StarcevAlexander@users.noreply.github.com>
2024-02-06 22:27:37 +0800

    some envirment 2

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  api: 'http://localhost:3000/api/',
-  serverStaticPath: 'http://localhost:3000/images/products/',
+  // api: 'http://localhost:3000/api/',
+  // serverStaticPath: 'http://localhost:3000/images/products/',
+  api: 'http://testforfun.ru/api/',
+  serverStaticPath: 'http://testforfun.ru/images/products/',
 };
